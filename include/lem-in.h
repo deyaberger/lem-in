16df7d8169767b1a68f8ded0a6a9ee1228dba028
Deya BERGER <dberger@e2r7p11.42.fr>
2019-09-23 14:14:34 +0200

    makefile done avec bons noms de fichiers

---
+++ include/lem-in.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem-in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/19 18:31:12 by ncoursol          #+#    #+#             */
+/*   Updated: 2019/09/23 12:49:20 by dberger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+# include "../lib/ft_printf/ft_printf.h"
+# include "../lib/libft/libft.h"
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+
+typedef struct		s_struct
+{
+	int				ant_nb;
+	int				room_nb;
+	int				pipe_nb;
+}					t_struct;
+
+typedef struct		s_lst
+{
+	char			*name;
+	int				id;
+	int				x;
+	int				y;
+	int				type;
+	int				*link;
+	struct s_lst	*next;
+}					t_lst;
+
+void			ft_test(int i);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/19 18:28:25 by ncoursol          #+#    #+#             */
+/*   Updated: 2019/09/23 12:48:20 by dberger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/lem-in.h"
+
+/*void			error_output()
+{
+	//free all
+	ft_printf("ERROR\n");
+}
+
+t_struct		init_var(t_struct t)
+{
+	t.ant_nb = 0;
+	t.room_nb = 0;
+	t.pipe_nb = 0;
+	return (t);
+}
+
+t_lst			init_lst(t_lst *l)
+{
+	if (!(l = (t_lst*)malloc(sizeof(*l))))
+		return (NULL);
+	l->id = 0;
+	l->x = 0;
+	l->y = 0;
+	l->type = 2;
+	l->next = NULL;
+	return (l);
+}
+*/
+
+int		main(void)
+{
+/*	char		*line;
+	t_struct	t;
+	t_lst		*l;
+
+	t = init_var(t);
+	l = init_lst(l); //free
+	storage(t, l);
+	algo(t, l);*/
+
+	int		i;
+
+	ft_test(332);
+	ft_printf("hola\n");
+	i = ft_atoi("1345");
+	ft_printf("i=%d\n", i);
+	return (0);
+}
