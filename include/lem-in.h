a35e1715ff269fcaea3fc60d12896397872154cf
Deya BERGER <dberger@e2r7p11.42.fr>
2019-09-23 17:15:53 +0200

    point h pour room et struct

--- include/lem-in.h
+++ include/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 18:31:12 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/09/23 15:21:31 by dberger          ###   ########.fr       */
+/*   Updated: 2019/09/23 17:14:38 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,18 @@ typedef struct		s_struct
 	int				ant_nb;
 	int				room_nb;
 	int				pipe_nb;
+	int				xmax;
+	int				ymax;
+	struct t_room	*first;
 }					t_struct;
 
-typedef struct		s_lst
+typedef struct		s_room
 {
 	char			*name;
 	int				type; 	//start(1) | end(2) | rien(0)
 	int				*link;	//liaisons
-	struct s_lst	*next;
-}					t_lst;
+	struct s_room	*next;
+}					t_room;
 
 int					ft_storage(t_struct *t, t_lst *l);
 
