7786bb2b1e19513d52b18badda323c141b1e00d9
Nicolas COURSOL <ncoursol@e2r7p12.42.fr>
2019-09-23 15:05:25 +0200

    ajout fct storage

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 18:28:25 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/09/23 14:39:46 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/09/23 15:01:11 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,32 @@ void			error_output()
 	ft_printf("ERROR\n");
 }
 
-t_struct		init_var(t_struct t)
+void			init_var(t_struct *t)
 {
-	t.line = NULL;
-	t.ant_nb = 0;
-	t.room_nb = 0;
-	t.pipe_nb = 0;
-	return (t);
+	t->line = NULL;
+	t->ant_nb = 0;
+	t->room_nb = 0;
+	t->pipe_nb = 0;
 }
 
-t_lst			init_lst(t_lst *l)
+void			init_lst(t_lst *l)
 {
 	if (!(l = (t_lst*)malloc(sizeof(*l))))
-	{
 		l->type = -1;
-		return (l);
-	}
 	l->type = 0;
 	l->next = NULL;
-	return (l);
 }
 
 int		main(void)
 {
 	t_struct	t;
-	t_lst		*l;
+	t_lst		l;
 
-	t = init_var(t);
-	if (l->type == -1)
+	init_var(&t);
+	init_lst(&l); //free
+	if (l.type == -1)
 		return (1);
-	l = init_lst(l); //free
-	storage(&t, l);
+	storage(&t, &l);
 //	algo(t, l);
 	return (0);
 }
--- src/storage.c
+++ src/storage.c
@@ -6,16 +6,20 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/20 10:37:36 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/09/23 13:45:53 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/09/23 15:02:53 by ncoursol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		storage(t_struct t, t_lst *l)
+#include "../include/lem-in.h"
+
+int		storage(t_struct *t, t_lst *l)
 {
-	
-
-
-
-
+	while (t->line[1] != '-' && t->line[0] != '#')
+	{
+		get_next_line(0, t->line);
+		ft_printf("line : [%s]", t->line);
+		free(t->line);
+	}
+	(void)l;
 	return (0);
 }
