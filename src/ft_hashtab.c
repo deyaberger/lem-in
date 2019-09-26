da4f47efff56cf25cecc8cf4155b964623fe0c80
Deya BERGER <dberger@e2r7p13.42.fr>
2019-09-26 17:56:53 +0200

    ft_hashtab a la norme

--- src/ft_hashtab.c
+++ src/ft_hashtab.c
@@ -6,17 +6,17 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 13:17:46 by dberger           #+#    #+#             */
-/*   Updated: 2019/09/25 19:38:17 by dberger          ###   ########.fr       */
+/*   Updated: 2019/09/26 17:56:25 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem-in.h"
 
-unsigned int		ft_hashage(char *name, int hash_size)
+unsigned int	ft_hashage(char *name, int hash_size)
 {
 	size_t			i;
 	unsigned int	hash;
-	
+
 	i = 0;
 	hash = 0;
 	while (i != ft_strlen(name))
@@ -31,48 +31,66 @@ unsigned int		ft_hashage(char *name, int hash_size)
 	return (hash % hash_size);
 }
 
-void		ft_init_tab(t_struct *t)
+void			ft_init_tab(t_struct *t, unsigned int s)
 {
 	unsigned int i;
 
 	i = 0;
-	while (i < t->room_nb * 10)
+	while (i < s)
 	{
 		t->tab[i] = NULL;
 		i++;
 	}
 }
 
-int			ft_hashtab(t_struct *t, t_room *r)
+int				ft_init_links_tab(t_struct *t, t_room *r, int i)
 {
-	unsigned int 	i;
-	
-	t->tab = malloc(sizeof(t->tab) * t->room_nb * 10);
-	ft_init_tab(t);
+	t->tab[i] = r;
+	if (!(t->tab[i]->link = (int*)malloc(sizeof(int) * t->room_nb + 1)))
+		return (0);
+	ft_bzero(t->tab[i]->link, t->room_nb);
+	t->tab[i]->link[0] = -1;
+	return (1);
+}
+
+int				ft_coal(t_struct *t, t_room *r, int i, int s)
+{
+	while (i < s && t->tab[i] != NULL && ft_strcmp(r->name, t->tab[i]->name))
+	{
+		i++;
+		if (i == s)
+			i = 0;
+	}
+	if (t->tab[i] == NULL)
+	{
+		if (!(ft_init_links_tab(t, r, i)))
+			return (0);
+	}
+	else if (!(ft_strcmp(r->name, t->tab[i]->name)))
+		return (0);
+	return (1);
+}
+
+int				ft_hashtab(t_struct *t, t_room *r)
+{
+	unsigned int	i;
+	unsigned int	s;
+
+	s = t->room_nb * 10;
+	t->tab = malloc(sizeof(t->tab) * s);
+	ft_init_tab(t, s);
 	r = t->first;
 	while (r)
 	{
-		i = ft_hashage(r->name, (t->room_nb * 10));
-		if (t->tab[i] == NULL && i < (t->room_nb * 10))
+		i = ft_hashage(r->name, s);
+		if (i < s && t->tab[i] == NULL)
 		{
-			t->tab[i] = r;
-			if (!(t->tab[i]->link = (int*)malloc(sizeof(int) * t->room_nb + 1)))
+			if (!(ft_init_links_tab(t, r, i)))
 				return (0);
-			ft_bzero(t->tab[i]->link, t->room_nb);
-			t->tab[i]->link[0] = -1;
 		}
 		else
 		{
-			while (i < (t->room_nb * 10) && t->tab[i] != NULL && ft_strcmp(r->name, t->tab[i]->name))
-			{
-				i++;
-				if (i == (t->room_nb * 10))
-					i = 0;
-			}
-
-			if (t->tab[i] == NULL)
-				t->tab[i] = r;
-			else if (!(ft_strcmp(r->name, t->tab[i]->name)))
+			if (!(ft_coal(t, r, i, s)))
 				return (0);
 		}
 		r = r->next;
