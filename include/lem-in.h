f8367f314fd393606bcbbb11b664248d2025b514
Deya BERGER <dberger@e2r3p13.42.fr>
2019-10-08 12:00:53 +0200

    .h a jour

--- include/lem-in.h
+++ include/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 18:31:12 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/10/01 15:24:51 by ncoursol         ###   ########.fr       */
+/*   Updated: 2019/10/04 20:10:36 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,47 @@ typedef struct		s_struct
 {
 	char			*line;
 	struct s_room	**tab;
+	struct s_room	*first;
+	struct s_room	*start;
+	struct s_room	*end;
+	int				**coord;
 	int				ant_nb;
 	unsigned int	room_nb;
-	int				pipe_nb;
+	unsigned int	link_nb;
 	int				xmax;
 	int				ymax;
-	int				start;
-	int				end;
-	int				max_paths;
-	int				**path;
-	int				**coord;
-	struct s_room	*first;
 }					t_struct;
 
 typedef struct		s_room
 {
 	char			*name;
-	int				type; 	//start(1) | end(2) | rien(0)
-	int				*link;	//liaisons
-	int				ant;
-	int				*hm;
-	int				nbl;
+	struct s_link	**ways;
 	struct s_room	*next;
+	struct s_room	*mum;
+	int				type; 	//start(1) | end(2) | rien(0)
+	int				weight;
+	int				q;
+	int				used;
 }					t_room;
+
+typedef struct		s_link
+{
+	struct s_room	*dest;
+	struct s_link	*rev;
+	int				status;	
+}					t_link;
+
+typedef struct		s_bfs
+{
+	struct s_room	**steps;
+	int				len;
+}					t_bfs;
 
 int					ft_storage(t_struct *t, t_room *r, int i);
 t_room				*init_room(t_room *r);
 unsigned int		ft_hashage(char *name, int hash_size);
 int					ft_hashtab(t_struct *t, t_room *r);
 int					ft_links(t_struct *t);
-int					ft_heat(t_struct *t);
+int					ft_bfs(t_struct *t, t_room *r);
 
 #endif
