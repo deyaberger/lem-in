a8ddabcf31425def77339b52e6c5a6d9c2fd25e4
Deya BERGER <dberger@e2r9p7.42.fr>
2019-10-16 17:47:01 +0200

    karp mais stockage et comparaison de chemins pas fait

--- src/ft_bfs.c
+++ src/ft_bfs.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 13:13:51 by dberger           #+#    #+#             */
-/*   Updated: 2019/10/09 20:51:31 by dberger          ###   ########.fr       */
+/*   Updated: 2019/10/16 17:45:55 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,23 +35,30 @@ t_room	*ft_new_in(t_room *r, t_room *queue, t_room **ngb, int weight)
 	return (queue);
 }
 
-t_room	*ft_put_weight(t_struct *t, t_room *r, t_room *queue)
+t_room	*ft_put_weight(t_struct *t, t_room *r, t_room *queue, int mode)
 {
 	t_room	*ngb;
 	int	i;
+	int	j;
 	int	new_weight;
 
 	i = 0;
+	j = 0;
 	ngb = NULL;
 	r->q = 1;
 	r->used = 1;
 	if (queue == NULL)
 		queue = r;
 	new_weight = 0;
+	(void)mode;
 	while (i < r->nbl)
 	{
 		ngb = r->ways[i]->dest;
-		if ((r != ngb->mum) && (ngb != t->start) && (((r->ways[i]->status == 0) && (r->opti == 0))
+		if (r->ways[i]->dest == r->mum)
+			j = i;
+		if ((ngb != r->mum) && (ngb != t->start)
+		&& ((((r->ways[i]->status == 0) && (r->opti == 0))
+		|| ((r->ways[i]->status == 0) && (r->opti == 1) && (r->ways[j]->status == 1)))
 		|| ((r->ways[i]->status == -1) && (r->opti == 1))))
 		{
 			if (ngb->used == 0 && ngb->q == 0)
@@ -72,8 +79,9 @@ t_room	*ft_init_var(t_room *r, t_room *queue)
 {
 	t_room *save;
 
+	queue = r;
 	save = queue;
-	while (queue && queue->next)
+	while (queue)
 	{
 		r = queue;
 		r->weight = 0;
@@ -92,7 +100,11 @@ int8_t	ft_bfs(t_struct *t, t_room *r)
 	t_room *queue;
 	t_ways	*w;
 	int	max_paths;
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
 	max_paths = 0;
 	queue = NULL;
 	w = ft_memalloc(sizeof(t_ways));
@@ -106,15 +118,65 @@ int8_t	ft_bfs(t_struct *t, t_room *r)
 		queue = ft_init_var(r, queue);
 	while (t->end->q == 0)
 	{
-		queue = ft_put_weight(t, r, queue);
+		queue = ft_put_weight(t, r, queue, 0);
 		r = r->next;
 	}
-	while (t->end->q == 1 && r->next && r != queue)
+	while (t->end->q == 1 && r)
 	{
 		if (r->weight < t->end->weight - 1)
-			queue = ft_put_weight(t, r, queue);
+			queue = ft_put_weight(t, r, queue, 0);
 		r = r->next;
 	}
-	ft_karp(t, r, w);
+	w = ft_karp(t, r, w);	
+
+///////////////////////////////////////////////////////
+
+	
+	r = t->start;
+	if (queue != NULL)
+		queue = ft_init_var(r, queue);
+	while (t->end->q == 0)
+	{
+		queue = ft_put_weight(t, r, queue, 1);
+		r = r->next;
+	}
+	while (t->end->q == 1 && r)
+	{
+		if (r->weight < t->end->weight - 1)
+			queue = ft_put_weight(t, r, queue, 2);
+		r = r->next;
+	}
+	ft_printf("\n************\n\n");
+	w = ft_karp(t, r, w);	
+
+///////////////////////////////////////////////////////
+
+
+	r = t->start;
+	if (queue != NULL)
+		queue = ft_init_var(r, queue);
+	while (t->end->q == 0)
+	{
+		queue = ft_put_weight(t, r, queue, 3);
+		r = r->next;
+	}
+	while (t->end->q == 1 && r)
+	{
+		if (r->weight < t->end->weight - 1)
+			queue = ft_put_weight(t, r, queue, 4);
+		r = r->next;
+	}
+	ft_printf("\n************\n\n");
+	w = ft_karp(t, r, w);	
+
+///////////////////////////////////////////////////////
+
+
+/*	while (w->steps[i][j])
+	{	
+		ft_printf("%s,", w->steps[i][j]->name);
+		j++;
+	}	
+	ft_printf("\n");*/
 	return (1);
 }
--- src/ft_karp.c
+++ src/ft_karp.c
@@ -6,7 +6,7 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 19:00:23 by dberger           #+#    #+#             */
-/*   Updated: 2019/10/09 20:51:45 by dberger          ###   ########.fr       */
+/*   Updated: 2019/10/16 17:44:51 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ t_ways		*ft_stock_steps(t_struct *t, t_room *r, t_ways *w)
 	m = 0;
 	while (i < t->start->nbl && w->steps)
 	{
-		if (r->ways[i]->status == 1)
+		if (r->ways[i]->status == 1 && r->ways[i]->dest->opti == 0)
 		{
 			while (w->steps[j] != NULL)
 				j++;
+			ft_printf("j= %d\n", j);
 			w->steps[j] = ft_memalloc(sizeof(t_room) * t->end->weight);
 			m = i;
 			while (r != t->end)
@@ -36,7 +37,8 @@ t_ways		*ft_stock_steps(t_struct *t, t_room *r, t_ways *w)
 				while (r->ways[m]->status != 1)
 					m++;
 				r = r->ways[m]->dest;
-				r->opti = 1;
+				if (r == t->end || (r != t->end && r->opti == 0))
+					r->opti = 1;
 				w->steps[j][k] = r;
 				k++;
 				m = 0;
@@ -51,29 +53,42 @@ t_ways		*ft_stock_steps(t_struct *t, t_room *r, t_ways *w)
 	return (w);
 }
 
-int8_t		ft_karp(t_struct *t, t_room *r, t_ways *w)
+t_ways		*ft_karp(t_struct *t, t_room *r, t_ways *w)
 {
 	int	i;
+	t_link	*link;
 	
 	i = 0;
 	r = t->end;
+	link = NULL;
 	while (r != t->start)
 	{
+		i = 0;
 		while (r->ways[i]->dest != r->mum)
 			i++;
-		if (r->ways[i]->status == 0 && r->ways[i]->rev->status == 0)
+		link = r->ways[i];
+		if (link->status == 0 && link->rev->status == 0)
 		{
-			r->ways[i]->status = -1;
-			r->ways[i]->rev->status = 1;
+			link->status = -1;
+			link->rev->status = 1;
 		}
-		else if (r->ways[i]->status == -1 && r->ways[i]->rev->status == 1)
+		else if (link->status == -1 && link->rev->status == 1)
 		{
-			r->ways[i]->status = 0;
-			r->ways[i]->rev->status = 0;
+			link->status = 0;
+			link->rev->status = 0;
 		}
 		r = r->mum;
 	}
 	w = ft_stock_steps(t, r, w);
-	return (0);
+	i = 0;
+	if (w->steps[2])
+	{
+		while (w->steps[2][i])
+		{
+			ft_printf("%s, ", w->steps[2][i]->name);
+			i++;
+		}	
+	}
+	return (w);
 		
 }
