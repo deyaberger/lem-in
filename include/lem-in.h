43a8ce28f72dd32053a34c38092745d0e34d54b9
Deya BERGER <dberger@e2r6p12.42.fr>
2019-10-22 15:22:42 +0200

    ft_weight a la norme avec des nouveaux define

--- include/lem-in.h
+++ include/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 18:31:12 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/10/21 17:35:19 by dberger          ###   ########.fr       */
+/*   Updated: 2019/10/22 15:07:59 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,14 @@
 #define BOOL int
 #define TRUE 1
 #define FALSE 0
+#define NO_MORE_STEPS 0
 #define ROOM_START 1
 #define ROOM_END 2
 #define ROOM_NORMAL 0
+#define GOOD_PATH 1
+#define CLEAN 2
+#define REVERSE 3
+#define CONTINUE_AFTER_REVERSE 4
 
 typedef struct		s_info
 {
@@ -45,12 +50,12 @@ typedef struct		s_room
 	struct s_link	**ways;
 	struct s_room	*next;
 	struct s_room	*mum;
+	size_t		weight;
+	size_t		nbl;
 	int		type; 	//start(1) | end(2) | rien(0)
-	int		weight;
 	int		opti;
 	int		q;
 	int		used;
-	size_t		nbl;
 }			t_room;
 
 
--- src/ft_weight.c
+++ src/ft_weight.c
@@ -6,15 +6,15 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 17:50:55 by dberger           #+#    #+#             */
-/*   Updated: 2019/10/22 12:28:50 by dberger          ###   ########.fr       */
+/*   Updated: 2019/10/22 15:21:40 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem-in.h"
 
-int	ft_calc_weight(t_room *room, size_t i)
+size_t	ft_calc_weight(t_room *room, size_t i)
 {
-	int	weight;
+	size_t	weight;
 
 	weight = 0;
 	if (room->ways[i]->status == -1)
@@ -43,23 +43,41 @@ BOOL	ft_check_link(t_room *ngb, t_info *info, t_room *room, size_t i)
 	link = room->ways[i];
 	if (ngb == room->mum)
 		mum = i;
-	if ((ngb != room->mum) && (ngb != info->start)
-	&& ((((link->status == 0) && (room->opti == 0))
-	|| ((link->status == 0) && (room->opti == 1)
-	&& (room->ways[mum]->status == 1)))
-	|| ((link->status == -1) && (room->opti == 1)) || ((link->status == 0) && (room->ways[mum]->status == -1) && (room->opti == 1) )))
-			return (TRUE);
+	if (ngb != room->mum && ngb != info->start)
+		i = GOOD_PATH;
+	if (room->opti == 0 && link->status == 0)
+		i += CLEAN;
+	if (room->opti == 1 && link->status == -1)
+		i += REVERSE;
+	if (room->opti == 1 && link->status == 0 && room->ways[mum]->status == -1)
+		i += CONTINUE_AFTER_REVERSE;
+	if (i == GOOD_PATH + CLEAN || i == GOOD_PATH + REVERSE
+			|| i == GOOD_PATH + CONTINUE_AFTER_REVERSE)
+		return (TRUE);
 	return (FALSE);
+}
+
+void	ft_add_to_queue(t_room *room, t_room **queue, t_room **ngb, size_t i)
+{
+	size_t new_weight;
+
+	new_weight = 0;
+	if ((*ngb)->used == 0 && (*ngb)->q == 0)
+		ft_new_in(room, queue, ngb, ft_calc_weight(room, i));
+	else if ((*ngb)->used == 1 || (*ngb)->q == 1)
+	{
+		new_weight = ft_calc_weight(room, i);
+		if (new_weight < (*ngb)->weight)
+			ft_new_in(room, queue, ngb, new_weight);
+	}
 }
 
 t_room	*ft_weight(t_info *info, t_room *room, t_room *queue)
 {
 	t_room	*ngb;
 	size_t	i;
-	int	new_weight;
 
 	i = 0;
-	new_weight = 0;
 	ngb = NULL;
 	room->q = 1;
 	room->used = 1;
@@ -69,16 +87,7 @@ t_room	*ft_weight(t_info *info, t_room *room, t_room *queue)
 	{
 		ngb = room->ways[i]->dest;
 		if (ft_check_link(ngb, info, room, i) == TRUE)
-		{
-			if (ngb->used == 0 && ngb->q == 0)
-				ft_new_in(room, &queue, &ngb, ft_calc_weight(room, i));
-			else if (ngb->used == 1 || ngb->q == 1)
-			{
-				new_weight = ft_calc_weight(room, i);
-				if (new_weight < ngb->weight)
-					ft_new_in(room, &queue, &ngb, new_weight);
-			}
-		}
+			ft_add_to_queue(room, &queue, &ngb, i);
 		i++;
 	}
 	return (queue);
