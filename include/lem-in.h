673934e26b967f45981f8552d0f59eac3462c640
Deya BERGER <dberger@e2r3p18.42.fr>
2019-11-19 17:10:38 +0100

    un beau lemin presque a la norme

--- include/lem-in.h
+++ include/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: ncoursol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 18:31:12 by ncoursol          #+#    #+#             */
-/*   Updated: 2019/11/18 14:05:23 by dberger          ###   ########.fr       */
+/*   Updated: 2019/11/19 17:08:18 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,106 +16,101 @@
 # include "../lib/ft_printf/ft_printf.h"
 # include "../lib/libft/libft.h"
 
-# define IN_Q	(1 << 1)
-# define REV	(1 << 2)
-
-# define GOOD_PATH	(1 << 3)
-# define CLEAN		(1 << 4)
-# define REVERSE	(1 << 5)
-# define OUT		(1 << 6)
-
-#define BOOL int
-#define BUF 200
-#define TRUE 1
-#define FALSE 0
-#define ROOM_START 1
-#define ROOM_END 2
-#define ROOM_NORMAL 0
-#define	UNUSED 0
-#define	BACKWARD -1
-#define	FORWARD 1
-#define	CANCELED 0
-#define	INIT 0
-#define	FINISH 1
-#define	LENGTH 0
-#define	ANTS 1
-#define	STEPS 2
-#define	ANTS_SENT 3
-#define	ANT_ARRIVED 4
-#define	NONE -1
-#define	NEVER_FILLED -1
-#define KEEP_SEARCHING 1
-#define STOP 0
+# define GOOD_PATH (1 << 1)
+# define CLEAN (1 << 2)
+# define REVERSE (1 << 3)
+# define OUT (1 << 4)
+# define BOOL int
+# define BUF 200
+# define TRUE 1
+# define FALSE 0
+# define ROOM_START 1
+# define ROOM_END 2
+# define ROOM_NORMAL 0
+# define UNUSED 0
+# define BACKWARD -1
+# define FORWARD 1
+# define CANCELED 0
+# define INIT 0
+# define FINISH 1
+# define LENGTH 0
+# define ANTS 1
+# define STEPS 2
+# define ANTS_SENT 3
+# define ANT_ARRIVED 4
+# define NONE -1
+# define NEVER_FILLED -1
+# define KEEP_SEARCHING 1
+# define STOP 0
+# define NOT_IN_QUEUE 0
+# define IN_QUEUE 1
 
 typedef struct		s_info
 {
-	char		*line;
+	char			*line;
 	struct s_room	**tab;
 	struct s_room	*first;
 	struct s_room	*start;
 	struct s_room	*end;
-	int		**coord;
-	int		stop_str;
-	int		len_str;
-	int		lines_rqd;
-	int		ant_nb;
-	int		max_paths;
-	int		room_nb;
-	int		link_nb;
-	int		xmax;
-	int		ymax;
-	int		fd;
-}			t_info;
+	int				**coord;
+	int				stop_str;
+	int				len_str;
+	int				lines_rqd;
+	int				ant_nb;
+	int				max_paths;
+	int				room_nb;
+	int				link_nb;
+	int				xmax;
+	int				ymax;
+}					t_info;
 
 typedef struct		s_room
 {
-	char		*name;
+	char			*name;
 	struct s_link	**link;
 	struct s_room	*next;
 	struct s_room	*mum;
-//	struct s_room	*mum2;
-	int		weight;
-	int		ant_index;
-	int		stat;
-	int		nbl;
-	int		type; 	//start(1) | end(2) | rien(0)
-	int		opti;
-}			t_room;
-
+	int				weight;
+	int				ant_index;
+	int				in_q;
+	int				nbl;
+	int				type; 	//start(1) | end(2) | rien(0)
+	int				opti;
+}					t_room;
 
 typedef struct		s_link
 {
 	struct s_room	*dest;
 	struct s_link	*rev;
-	int		status;
-}			t_link;
+	int				status;
+}					t_link;
 
 typedef struct		s_ways
 {
 	struct s_room	***steps;
-	int		**path_info;
-	int		tot_max;
-	int		min;
-	int		nb_ways;
-	int		tot_pl;
-}			t_ways;
+	int				**path_info;
+	int				tot_max;
+	int				min;
+	int				nb_ways;
+	int				tot_pl;
+}					t_ways;
 
-void			error_exit(int nb, char *str);
-char			*ft_strjoin_nf(char *s1, char *s2, int a, t_info *info);
-t_room			*init_room(void);
-BOOL			ft_storage(t_info *t, t_room *r, int i, char **str);
-int				ft_coll(t_info *info, char *name, int i, int s);
-int				ft_hashage(char *name, int hash_size);
-void			ft_hashtab(t_info *t, t_room *r);
-BOOL			ft_links(t_info *t, char **str);
-t_room			*ft_weight(t_info *t, t_room *r, t_room *queue);
-void			ft_init_ways(t_ways **ways);
-void			ft_clean_steps(t_ways *ways, int mode);
-void			ft_clean_free(t_info *info);
-t_ways			*ft_calc_steps(t_ways *ways, t_info *info, int j);
-t_ways			*ft_steps(t_info *t, t_room *r, t_ways *comp);
-t_ways			ft_bfs(t_info *t, t_room *r);
-BOOL			ft_karp(t_info *t, t_room *r, t_ways *best, t_ways *comp);
-void			ft_result(char *str, t_info info, t_ways *best);
+void				error_exit(int nb, char *str);
+char				*ft_strjoin_nf(char *s1, char *s2, int a, t_info *info);
+t_room				*init_room(void);
+BOOL				ft_storage(t_info *t, t_room *r, int i, char **str);
+int					ft_coll(t_info *info, char *name, int i, int s);
+int					ft_hashage(char *name, int hash_size);
+void				ft_hashtab(t_info *t, t_room *r);
+BOOL				ft_links(t_info *t, char **str);
+t_room				*ft_weight(t_info *t, t_room *r, t_room *queue);
+void				ft_init_ways(t_ways **ways);
+void				ft_clean_steps(t_ways *ways, int mode);
+void				ft_clean_free(t_info *info);
+t_ways				*ft_calc_steps(t_ways *ways, t_info *info, int j);
+t_ways				*ft_steps(t_info *t, t_room *r, t_ways *comp);
+t_ways				ft_bfs(t_info *t, t_room *r);
+BOOL				ft_karp(t_info *t, t_room *r, t_ways *best, t_ways *comp);
+void				ft_result(char *str, t_info info, t_ways *best);
 
 #endif
