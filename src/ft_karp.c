bcbf5b7b5f6c3feb6ce51225267988fc2d1b63d7
Deya BERGER <dberger@e2r8p12.42.fr>
2019-11-20 13:44:21 +0100

    ft_karp a la norme

--- src/ft_karp.c
+++ src/ft_karp.c
@@ -6,11 +6,31 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 19:00:23 by dberger           #+#    #+#             */
-/*   Updated: 2019/11/18 16:54:54 by dberger          ###   ########.fr       */
+/*   Updated: 2019/11/20 13:44:06 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem-in.h"
+
+t_ways	*ft_copy_comp(int j, t_ways *best, t_ways *comp)
+{
+	int k;
+
+	k = 0;
+	best->nb_ways = comp->nb_ways;
+	best->tot_max = comp->tot_max;
+	best->tot_pl = comp->tot_pl;
+	best->min = comp->min;
+	while (comp->steps[j][k])
+	{
+		if (k < 5)
+			best->path_info[j][k] = comp->path_info[j][k];
+		comp->steps[j][k]->opti = 1;
+		best->steps[j][k] = comp->steps[j][k];
+		k++;
+	}
+	return (best);
+}
 
 void	ft_new_best(t_ways *best, t_ways *comp)
 {
@@ -21,30 +41,12 @@ void	ft_new_best(t_ways *best, t_ways *comp)
 	k = 0;
 	while (comp->steps[j] && j <= comp->nb_ways)
 	{
-		while (comp->steps[j][k])
-			k++;
+		k = comp->path_info[j][LENGTH];
 		if (!(best->steps[j] = ft_memalloc(sizeof(t_room) * k)))
 			error_exit(12, "Can't malloc best->steps");
 		if (!(best->path_info[j] = ft_memalloc(sizeof(int) * 5)))
 			error_exit(12, "Can't malloc best->path_info[j]");
-		k = 0;
-		best->nb_ways = comp->nb_ways;
-		best->tot_max = comp->tot_max;
-		best->tot_pl = comp->tot_pl;
-		best->min = comp->min;
-		while (k < 5)
-		{
-			best->path_info[j][k] = comp->path_info[j][k];
-			k++;
-		}
-		k = 0;
-		while (comp->steps[j][k])
-		{
-			comp->steps[j][k]->opti = 1;
-			best->steps[j][k] = comp->steps[j][k];
-			k++;
-		}
-		k = 0;
+		best = ft_copy_comp(j, best, comp);
 		j++;
 	}
 }
@@ -52,7 +54,7 @@ void	ft_new_best(t_ways *best, t_ways *comp)
 void	ft_update_status(t_room *room)
 {
 	t_link	*link;
-	int	i;
+	int		i;
 
 	i = 0;
 	link = NULL;
@@ -83,14 +85,16 @@ BOOL	ft_karp(t_info *info, t_room *room, t_ways *best, t_ways *comp)
 		best = ft_steps(info, room, best);
 	else
 		comp = ft_steps(info, room, comp);
-	if (comp->nb_ways != NEVER_FILLED && comp->tot_max < best->tot_max && comp->tot_max != -1)
+	if (comp->nb_ways != NEVER_FILLED
+		&& comp->tot_max < best->tot_max && comp->tot_max != -1)
 	{
 		ft_clean_steps(best, 0);
 		ft_new_best(best, comp);
 		ft_clean_steps(comp, 2);
 		return (KEEP_SEARCHING);
 	}
-	else if (comp->nb_ways != NEVER_FILLED && (comp->tot_max > best->tot_max || comp->tot_max == -1))
+	else if (comp->nb_ways != NEVER_FILLED
+		&& (comp->tot_max > best->tot_max || comp->tot_max == -1))
 	{
 		ft_clean_steps(comp, 2);
 		return (STOP);
