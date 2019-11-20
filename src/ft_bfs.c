85726063a6e1063dc25b27c911079d2c8a75c978
Deya BERGER <dberger@e2r8p12.42.fr>
2019-11-20 13:29:06 +0100

    ft_bfs a la norme

--- src/ft_bfs.c
+++ src/ft_bfs.c
@@ -6,13 +6,13 @@
 /*   By: dberger <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 16:59:22 by dberger           #+#    #+#             */
-/*   Updated: 2019/11/19 18:11:36 by dberger          ###   ########.fr       */
+/*   Updated: 2019/11/20 13:28:51 by dberger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem-in.h"
 
-t_room		*ft_init_var(t_room *room, t_room *queue)
+t_room	*ft_init_var(t_room *room, t_room *queue)
 {
 	t_room	*save;
 
@@ -32,26 +32,19 @@ t_room		*ft_init_var(t_room *room, t_room *queue)
 	return (queue);
 }
 
-t_ways		ft_init_steps(int size)
+t_ways	ft_init_steps(int size)
 {
 	t_ways	ways;
 
 	if (!(ways.steps = ft_memalloc(sizeof(t_room*) * size)))
 		error_exit(7, "Can't malloc t_ways->steps");
 	ways.nb_ways = NEVER_FILLED;
+	if (!(ways.path_info = ft_memalloc(sizeof(int*) * size)))
+		error_exit(11, "Can't malloc best.way_info");
 	return (ways);
 }
 
-void		ft_shorter_way(t_info *info, t_room **room, t_room **queue)
-{
-	while (info->end->in_q == NOT_IN_QUEUE && *room)
-	{
-		*queue = ft_weight(info, *room, *queue);
-		*room = (*room)->next;
-	}
-}
-
-void		ft_finish_queue(t_info *info, t_room **room, t_room **queue)
+void	ft_finish_queue(t_info *info, t_room **room, t_room **queue)
 {
 	while (*room && info->end->in_q == IN_QUEUE)
 	{
@@ -65,38 +58,44 @@ void		ft_finish_queue(t_info *info, t_room **room, t_room **queue)
 	}
 }
 
+BOOL	ft_shorter_way(t_info *info, t_room **room, t_room **queue)
+{
+	(*room)->in_q = IN_QUEUE;
+	while (info->end->in_q == NOT_IN_QUEUE && *room)
+	{
+		*queue = ft_weight(info, *room, *queue);
+		*room = (*room)->next;
+	}
+	if (info->end->in_q == NOT_IN_QUEUE)
+		return (STOP);
+	ft_finish_queue(info, room, queue);
+	return (KEEP_SEARCHING);
+}
+
 t_ways	ft_bfs(t_info *info, t_room *room)
 {
 	t_room	*queue;
 	t_ways	best;
 	t_ways	comp;
-	int	i;
+	int		i;
 
 	i = 0;
 	queue = NULL;
 	best = ft_init_steps(info->max_paths);
 	comp = ft_init_steps(info->max_paths);
-	if (!(best.path_info = ft_memalloc(sizeof(int*) * info->max_paths)))
-		error_exit(11, "Can't malloc best.way_info");
-	if (!(comp.path_info = ft_memalloc(sizeof(int*) * info->max_paths)))
-		error_exit(11, "Can't malloc comp.way_info");
 	while (i < info->max_paths && i < info->ant_nb)
 	{
 		room = info->start;
 		if (queue != NULL)
 			queue = ft_init_var(room, queue);
-		room->in_q = 1;
-		ft_shorter_way(info, &room, &queue);
-		if (info->end->in_q == NOT_IN_QUEUE)
+		if (ft_shorter_way(info, &room, &queue) == STOP)
 			return (best);
-		ft_finish_queue(info, &room, &queue);
-		if (ft_karp(info, room, &best, &comp) == KEEP_SEARCHING)
-			i++;
-		else
+		if (ft_karp(info, room, &best, &comp) == STOP)
 		{
 			ft_clean_steps(&comp, 1);
 			return (best);
 		}
+		i++;
 	}
 	ft_clean_steps(&comp, 1);
 	return (best);
